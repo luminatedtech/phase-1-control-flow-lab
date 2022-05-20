850ca134383ae8795e6d0c298252ee7ca5f536a2
avalmercafe02@gmail.com <allenkim@Allens-MacBook-Pro.local>
2022-05-19 22:55:18 -0400

    commit tree 0795b16cd0d1eb8210d1f3415662048f1d77ad11

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(rideLength){
+  switch (true) {
+    case rideLength <= 400:
+      return "This one is on me!"
+      break;
+    case rideLength>400 && rideLength<2000:
+      return "That will be twenty bucks."
+      break;
+    case rideLength>2000 && rideLength<2500:
+      return "I will gladly take your thirty bucks."
+      break;
+    case rideLength>2500:
+      return "No can do."
+      break;
+
+  }
+ console.log(scuberGreetingForFeet(2600))
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+currentCity ="SFC"
+function ternaryCheckCity(currentCity){
+  const messageCity = (currentCity === "NYC") ? "Ok, sounds good." : "No go."
+  return messageCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+const tipAmmount ="not as generous"
+function switchOnCharmFromTip(tipAmmount){
+  switch (tipAmmount) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
